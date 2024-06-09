62a3d677c9eb77a34acdd3421833fee53fa36a13
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-06-09 21:39:51 +0600

    Practicing nested route

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,13 @@
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
         <div>
             <nav>
-                <ul>
-                    <a href="/">Home</a>
-                    <a href="/About">About</a>
-                    <a href="/Contact">Contact</a>
+                <ul style={{display:"flex", gap:"20px"}}>
+                    <Link to={'/'}>Home</Link>
+                    <Link to={'/About'}>About</Link>
+                    <Link to={'/Contact'}>Contact</Link>
                 </ul>
             </nav>
         </div>
