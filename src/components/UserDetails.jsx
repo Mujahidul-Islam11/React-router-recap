cbc45edc6dbc44fbdf84363917ecf3d71a15ba8a
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-06-09 23:42:43 +0600

    Added a go back button using useNavigate hook

--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -1,11 +1,13 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 
 
 const UserDetails = () => {
     const user = useLoaderData();
+    const navigate = useNavigate();
     return (
         <div>
             <h2>Username: {user?.name}</h2>
+            <button onClick={()=> navigate(-1)}>Go back</button>
         </div>
     );
 };
