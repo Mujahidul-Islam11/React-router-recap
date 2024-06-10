16127f668a64d025ffb916f9c5bcb8fcbe6e05cc
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-06-10 21:31:39 +0600

    Showed the user id by using useParams hook

--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -1,11 +1,14 @@
-import { useLoaderData, useNavigate } from "react-router-dom";
+import { useLoaderData, useNavigate, useParams } from "react-router-dom";
 
 
 const UserDetails = () => {
     const user = useLoaderData();
     const navigate = useNavigate();
+    const params = useParams();
+    console.log(params)
     return (
         <div>
+            <h2>{params?.id}</h2>
             <h2>Username: {user?.name}</h2>
             <button onClick={()=> navigate(-1)}>Go back</button>
         </div>
